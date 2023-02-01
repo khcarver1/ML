84deeb24aa4319e578cb73b8157043c9c29e32c3
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2023-02-01 15:31:03 -0700

    Updates

---
+++ precache-manifest.1e45f0c8f3da9ab1e0dfec65869ad417.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8b1f59829dec21c5e81",
+    "url": "/ML/static/css/main.4b539411.chunk.css"
+  },
+  {
+    "revision": "c8b1f59829dec21c5e81",
+    "url": "/ML/static/js/main.c8b1f598.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/ML/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/ML/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "ea93e5633320a2bcf966",
+    "url": "/ML/static/js/runtime~main.ea93e563.js"
+  },
+  {
+    "revision": "4d2a646f6eb300b6d574c2be924216f0",
+    "url": "/ML/static/media/logo.4d2a646f.jpg"
+  },
+  {
+    "revision": "c31e7e9c70fe3285bda74c95eeb6adef",
+    "url": "/ML/static/media/pexels-simone-defendi-12762122.c31e7e9c.jpg"
+  },
+  {
+    "revision": "0c7a72c0484e794b2f80de7de6c7362f",
+    "url": "/ML/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f3683c76a548094f556f644bc2939a4.js"
+  "/ML/precache-manifest.1e45f0c8f3da9ab1e0dfec65869ad417.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ML/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
