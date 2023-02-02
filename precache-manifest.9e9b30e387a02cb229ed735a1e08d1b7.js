80cb63fcff72d600ac4e4ac0da32eea3f9f3d40b
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2023-02-02 16:40:40 -0700

    Updates

---
+++ precache-manifest.9e9b30e387a02cb229ed735a1e08d1b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a182332273919dee241",
+    "url": "/static/css/main.55ba01aa.chunk.css"
+  },
+  {
+    "revision": "8a182332273919dee241",
+    "url": "/static/js/main.8a182332.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "340326b5f9d0a2124308",
+    "url": "/static/js/2.340326b5.chunk.js"
+  },
+  {
+    "revision": "1458782b48af82eb3a96",
+    "url": "/static/js/runtime~main.1458782b.js"
+  },
+  {
+    "revision": "4d2a646f6eb300b6d574c2be924216f0",
+    "url": "/static/media/logo.4d2a646f.jpg"
+  },
+  {
+    "revision": "515fc584baa97d0aae930bc6161a864f",
+    "url": "/static/media/shopping-cart.515fc584.png"
+  },
+  {
+    "revision": "c31e7e9c70fe3285bda74c95eeb6adef",
+    "url": "/static/media/pexels-simone-defendi-12762122.c31e7e9c.jpg"
+  },
+  {
+    "revision": "f79c5b502a29af970fc6d2e5bf955878",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f74e734941f173fef2cd7e0c8b6e9a6.js"
+  "/precache-manifest.9e9b30e387a02cb229ed735a1e08d1b7.js"
 );
 
 workbox.clientsClaim();
