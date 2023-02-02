c2991ba53a8cf48ab2aba22eca5479960cb473c1
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2023-02-02 16:25:33 -0700

    shop page updates

--- src/components/shop.js
+++ src/components/shop.js
@@ -1,5 +1,8 @@
 import logo from "../assets/logo.jpg";
 import React from "react";
+import Cart from "../assets/shopping-cart.png"
+import Search from "../assets/search.png"
+
 
 
 const Shop = (props) => {
@@ -7,6 +10,13 @@ const Shop = (props) => {
     <>
       <div className="App">
         <div className='mainContent'>
+          <div className="shopHdr">
+
+            <a href="/#/shop"><img src={Cart} width="25" /></a>
+            <a href="/#/shop"><img src={Search} width="25" /></a>
+
+          </div>
+
           <div>
             <p>Welcome to our shop.</p>
             <p>One day you'll see things for sale here</p>
@@ -17,5 +27,15 @@ const Shop = (props) => {
     </>
   );
 };
+
+
+// shopify dev //
+// var express = require('sys')
+//   , shopify = require('shopify');
+
+// var session = new shopify.Session(
+//     API_KEY
+//   , SHARED_SECRET
+// );
 
 export default Shop;
