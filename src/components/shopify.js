9818662c7a85110d7a54a8a28aa3a667f12d1771
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2023-02-02 17:06:48 -0700

    shop to shopify

---
+++ src/components/shopify.js
@@ -0,0 +1,41 @@
+import logo from "../assets/logo.jpg";
+import React from "react";
+import Cart from "../assets/shopping-cart.png"
+import Search from "../assets/search.png"
+
+
+
+const Shopify = () => {
+  return (
+    <>
+      <div className="App">
+        <div className='mainContent'>
+          <div className="shopifyHdr">
+
+            <a href="/#/shopify"><img src={Cart} width="25" /></a>
+            <a href="/#/shopify"><img src={Search} width="25" /></a>
+
+          </div>
+
+          <div className="shopMainContent">
+            <p>Welcome to our shopify.</p>
+            <p>One day you'll see things for sale here</p>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+
+// shopify dev //
+// var express = require('sys')
+//   , shopify = require('shopify');
+
+// var session = new shopify.Session(
+//     API_KEY
+//   , SHARED_SECRET
+// );
+
+export default Shopify;
